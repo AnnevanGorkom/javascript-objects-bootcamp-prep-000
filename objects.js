e1a43b96f6184db51c979baf1b2af2722f08341a
Anne van Gorkom <anne@vangorkom.com>
2018-01-15 12:01:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
 var playlist = {"London Grammar": "Oh Woman Oh Man", "Kanye": "Gold Digger"};
 
 function updatePlaylist(object, artiest, titel) {
-  object[artiest] = titel;
+  object["artiest"] = titel;
   return object;
 }
 
 function removeFromPlaylist(object, artiest) {
-  delete object.artiest;
+  delete object["artiest"];
   return object;
 }
